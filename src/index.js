0696bc53b809a226f87a2629e8fca0400b1bf8b6
Julian Kaindl <kaindl745@gmail.com>
2019-07-12 02:36:03 +0200

    Fix Position and Perspective

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'purecss';
-import { PerspectiveCamera, AmbientLight, Scene, WebGLRenderer, ConeGeometry } from 'three';
+import { PerspectiveCamera, PointLight, Scene, WebGLRenderer } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 //import { WebGLRenderer } from 'three/src/renderers/WebGLRenderer';
 //import { Scene } from 'three/src/scenes/Scene';
@@ -13,26 +13,18 @@ animate();
 
 function init() {
 
-    camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight);
+    camera = new PerspectiveCamera();
     camera.position.z = 1;
+    camera.position.y = 0.25;  
 
     scene = new Scene();
-
-    let light = new AmbientLight("#ffffff", 100);
-    scene.add(light);
 
     new GLTFLoader().load(
         'vocality-logo.glb',
         (gltf) => {
-
             logo = gltf.scene;
-
-            logo.scale.x = 5;
-            logo.scale.y = 5;
-            logo.scale.z = 5;
-
+            logo.scale.set(5, 5, 5);
             scene.add(logo);
-
         },
         (xhr) => {
             console.log((xhr.loaded / xhr.total * 100) + '% loaded');
@@ -45,7 +37,6 @@ function init() {
     renderer = new WebGLRenderer({ canvas: document.getElementById('three-canvas') });
     renderer.setSize(500, 500);
     renderer.setClearColor(0x252525, 1);
-    //document.body.appendChild(renderer.domElement);
 }
 
 function animate() {

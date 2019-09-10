00a91c0ea0b6f1b0ebfba2f62cb29e96640f4a7c
Julian Kaindl <kaindl745@gmail.com>
2019-09-10 20:57:50 +0200

    Fix color of drop-caps on faq page

--- src/components/FaqSegment.vue
+++ src/components/FaqSegment.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="faq-seg">
         <div class="question flex items-center h3">
-            <span class="drop-cap pl2 pb1 mr3">Q.</span>
+            <span class="drop-cap-q pl2 pb1 mr3">Q.</span>
             <p>
                 <b><slot name="question"></slot></b>
             </p>
         </div>
         <div class="answer flex items-center h4">
-            <span class="drop-cap pl2 pt1 mr3">A.</span>
+            <span class="drop-cap-a pl2 pt1 mr3">A.</span>
             <p>
                 <slot name="answer"></slot>
             </p>
@@ -20,20 +20,19 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-.drop-cap {
+.drop-cap-q {
     line-height: 72px;
     font-size: 72px;
     border-left: 4px solid;
+    color: clr(brand, pink);
 }
-.question {
-    .drop-cap {
-        color: clr(brand, pink);
-    }
+.drop-cap-a {
+    line-height: 72px;
+    font-size: 72px;
+    border-left: 4px solid;
+    color: #fff;
 }
 .answer {
     color: clr(text, secondary);
-    .dropCap {
-        color: clr(text, secondary);
-    }
 }
 </style>
